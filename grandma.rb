ca8abdbcd48a846e856d6e5ad25ebfa9166187e7
Arrington Perry <perryceo21@hotmail.com>
2020-04-03 01:39:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,2 +1,9 @@
-def speak_to_grandma
-  
+def speak_to_grandma(phrase)
+  if phrase=="I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase.upcase !=phrase
+    return "HUH?! SPEAK UP, SONNY!"
+  else 
+    return "NO, NOT SINCE 1938!"
+  end
+end 
